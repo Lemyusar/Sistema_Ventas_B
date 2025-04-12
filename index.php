2fb698529e5b4f3c16b2aaae72fef74475219d74
YuDiCC <exol12wuyifan@gmail.com>
2025-04-11 21:59:04 -0600

    Frontend cambio 1

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include 'Layout/header.php'; ?>
+
+<style>
+    main{
+        background-color: aqua
+    }
+
+    p{
+        background-color: salmon
+    }
+</style>
+
+<main>
+  <h2>Bienvenido a mi página de inicio</h2>
+  <p>Contenido principal aquí.</p>
+</main>
+
+<?php include 'Layout/footer.php'; ?>
